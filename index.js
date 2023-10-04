c8561578fe22b1d5af964d2f306fdb2e5fbd496e
g0rnn <kimgyunho2284@gmail.com>
2023-10-05 01:18:31 +0900

    third

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
+const bodyParser = require("body-parser");
 const config = require("./config/key");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+//application/x-wwwform-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json
+app.use(bodyParser.json());
+
+const { User } = require("./models/User");
 
 const mongoose = require("mongoose");
+
 mongoose
   .connect(config.mongoURI, {
     useNewUrlParser: true,
@@ -18,6 +23,23 @@ mongoose
   })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(`Example app listening on port ${port}!`));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    const userInfo = await user.save();
+    res.status(200).json({
+      success: true,
+    });
+  } catch (err) {
+    console.error("User Registration Error: ", err);
+    res.status(500).json({ success: false, error: err.message });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
