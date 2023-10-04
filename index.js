c50d811fdb797ab11fe9384eb28609d2bb97fb84
g0rnn <kimgyunho2284@gmail.com>
2023-10-04 21:57:34 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -2,21 +2,20 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const config = require("./config/key");
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://gyunh01:user1234@cluster0.64hubyb.mongodb.net/?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreatIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreatIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(`Example app listening on port ${port}!`));
 
